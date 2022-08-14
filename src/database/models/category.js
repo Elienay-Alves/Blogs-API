fa9323b8b00a1f35bb3a19f433de558174eba3f1
Elienay-Alves <elienay8486@gmail.com>
2022-08-14 14:44:49 -0300

    migrations e models criados

---
+++ src/database/models/category.js
@@ -0,0 +1,24 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: DataTypes.STRING,
+  },
+    {
+      timestamps: 'false',
+    });
+
+  Category.associate = (models) => {
+    Category.belongsToMany(models.User, {
+      foreignKey: 'userId', as: 'user'
+    });
+  };
+
+  return Category;
+}
+
+module.exports = Category;
---
+++ src/database/models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+    {
+      timestamps: 'false',
+    });
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'posts'
+    });
+  };
+
+  return User;
+
+}
+
+module.exports = User;
